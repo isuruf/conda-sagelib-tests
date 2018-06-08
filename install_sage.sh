1cdb416c0ac1f4a26941b923c4b9e65d5e73fe92
Isuru Fernando <isuruf@gmail.com>
2018-06-07 18:47:54 -0600

    circleci config

---
+++ install_sage.sh
@@ -0,0 +1,3 @@
+conda config --add channels conda-forge
+conda create -n sage sage=8.2 python=2 sphinx jmol pillow twisted gxx_linux-64
+source activate sage
---
+++ test_sage.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/sagemath/sage.git sagemath
+cd sagemath
+git checkout 8.2
+sage -tp src/doc/en/prep/Advanced-2DPlotting.rst > doctest.txt || true
+conda list '^sagelib$'
+conda list '^sage$'
+cat doctest.txt | grep '^sage -t ' | grep '#' > doctest.summary.txt || true
+conda install -y bc
+echo `cat doctest.summary.txt | awk '{ print $5 }' | sort | grep -E '[0-9]+' | paste -sd+ | bc` failing individual doctests
+cat doctest.summary.txt | awk '{ print $5 }' | sort | grep -vE '[0-9]+' | sort | uniq -c || true
