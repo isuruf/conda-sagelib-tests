365d7178bf28df36803aba8499259062e9e85260
Isuru Fernando <isuruf@gmail.com>
2018-06-17 13:38:26 -0600

    Remove workarounds

--- install_sage.sh
+++ install_sage.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-/usr/bin/sudo -n yum install -y libXtst
-
 source /opt/docker/bin/entrypoint_source
 conda config --add channels conda-forge
-conda create -n sage sage=8.2 python=2 sphinx jmol pillow twisted openjdk>8.0.121
+conda create -n sage sage=8.2 python=2
 
