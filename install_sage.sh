b8a7976de1dd4784d01e9a61b338865099fc09c3
Isuru Fernando <isuruf@gmail.com>
2018-06-17 13:07:38 -0600

    openjdk

--- install_sage.sh
+++ install_sage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/usr/bin/sudo -n yum install -y liberation-sans-fonts libXtst
+/usr/bin/sudo -n yum install -y libXtst
 
 source /opt/docker/bin/entrypoint_source
 conda config --add channels conda-forge
-conda create -n sage sage=8.2 python=2 sphinx jmol pillow twisted>=18.4
+conda create -n sage sage=8.2 python=2 sphinx jmol pillow twisted openjdk>8.0.121
 
