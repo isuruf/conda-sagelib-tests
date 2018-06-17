2d19c0c4a1818009cb8bac5aa261ea30826c1675
Isuru Fernando <isuruf@gmail.com>
2018-06-17 08:39:18 -0600

    Fix sudo line

--- install_sage.sh
+++ install_sage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum install -y liberation-sans-fonts libXtst
+/usr/bin/sudo -n yum install -y liberation-sans-fonts libXtst
 
 source /opt/docker/bin/entrypoint_source
 conda config --add channels conda-forge
